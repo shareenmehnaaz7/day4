2413c026c3a9c8d0108a55df6ef757a8330f29b0
shareenmehnaaz7 <60655216+shareenmehnaaz7@users.noreply.github.com>
2021-09-20 17:32:55 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,29 @@
+var choice=5;
+    switch (choice) {
+        case 0:
+            console.log("Monday");
+            break;
+        case 1:
+            console.log("Tuesday");
+            break;
+        case 2:
+            console.log("Wednesday");
+            break;
+        case 3:case 0:
+            console.log("Monday");
+            break;
+            console.log("Thursday");
+            break;
+        case 4:
+            console.log("Friday");
+            break;
+        case 5:
+            console.log("Saturday");
+            break;
+        case 6:
+            console.log("Sunday");
+            break;
+        default: 
+            console.log("hay ");
+            break;
+    }
